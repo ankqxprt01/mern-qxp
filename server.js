1cf06055ece77f01907762dd7ccbf90ebb7e014e
ankqxprt01 <ankitquickxpert@gmail.com>
2025-07-23 12:27:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const path = require("path")
 // const addTestUser = require('./utils/seedUsers');
 
 dotenv.config();
@@ -18,17 +16,12 @@ app.get('/', (req, res) => res.send('API Running'));
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes); //
 
-// Call the user insertion function
-// addTestUser();
-
 // static files
 app.use(express.static(path.join(__dirname,'./frontend/build')))
 
-const frontendPath = path.join(__dirname, './frontend/build');
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', { root: frontendPath });
-});
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
