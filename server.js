1d827a40b6bbe41b7bd60daefc3a9f27d1759342
ankqxprt01 <ankitquickxpert@gmail.com>
2025-07-23 23:58:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname,'./client/build')))
 
 // this only works in express 5
 app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 
