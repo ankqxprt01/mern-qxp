ff459359a67c0e20678d693f3789951fac15b3f9
ankqxprt01 <ankitquickxpert@gmail.com>
2025-07-23 21:25:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ const path = require("path")
 app.use(express.static(path.join(__dirname,'./frontend/build')))
 
 // this is in old express
-app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
-})
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
 
 // this only works in express 5
 // app.get(/.*/, function(req, res) {
