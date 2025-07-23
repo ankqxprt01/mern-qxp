47e9dd4c610565e0caa6778c1ba77e500b9e9084
ankqxprt01 <ankitquickxpert@gmail.com>
2025-07-23 12:14:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,11 @@ app.use('/api/users', userRoutes); //
 // static files
 app.use(express.static(path.join(__dirname,'./frontend/build')))
 
-app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
-})
+const frontendPath = path.join(__dirname, './frontend/build');
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', { root: frontendPath });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
